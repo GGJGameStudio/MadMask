8658335b0b2c649a6910913e906c59082929d490
fHarr <florian.harreau@outlook.com>
2020-05-08 10:51:54 +0200

    Add basic move for main character

--- Scenes/MainCharacter.cs
+++ Scenes/MainCharacter.cs
@@ -3,19 +3,31 @@ using System;
 
 public class MainCharacter : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+    private const float DeadZone = 0.2f;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
+    private Vector2 velocity = new Vector2();
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    [Export] 
+    public int speed = 200;
+
+    public void UpdateVelocity()
+    {
+        velocity = new Vector2();
+
+        var xAxis = Input.GetJoyAxis(0, (int)JoystickList.Axis0);
+
+        if (Mathf.Abs(xAxis) > DeadZone)
+        {
+            velocity.x += xAxis;
+        }
+
+        velocity = velocity * speed;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        this.UpdateVelocity();
+
+        velocity = this.MoveAndSlide(velocity);
+    }
 }
