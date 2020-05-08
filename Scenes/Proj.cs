821b04fbc6e60192ea676c5978aaac3e5870eba5
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2020-05-08 18:00:49 +0200

    proj

---
+++ Scenes/Proj.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class Proj : Area2D
+{
+    private float speed = 200;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+ // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        Position += new Vector2(Mathf.Cos(Rotation), Mathf.Sin(Rotation)) * speed * delta;
+    }
+}
